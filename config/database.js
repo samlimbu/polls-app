4417a664d42f6f9a365fcea13b9c1852339b8370
samlimbu <samlimbu@hotmail.com>
2018-02-25 18:57:40 +0545

    acorreted flash mesasge errors

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports={
-     database: "mongodb://localhost:27017/polls-app",
+     database: "mongodb://admin:admin@ds012188.mlab.com:12188/samlimbu",
      secret: "secret"
 }
 //database: "mongodb://admin:admin@ds012188.mlab.com:12188/samlimbu",
