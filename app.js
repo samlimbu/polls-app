f6e726ce1b48ba4ecf4c2f68c110096a5a3e030a
samlimbu <samlimbu@hotmail.com>
2018-02-25 17:35:48 +0545

    modify port

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname,'public/index.html'));
 })
 //heroku port: process.env.PORT || 8080
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, ()=>{
      console.log('server started on port' + port);
 });
