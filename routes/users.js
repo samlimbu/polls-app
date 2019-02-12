c2e6e42b8b85701d5e83ff776c6436b9a2b03d64
samlimbu <33290648+samlimbu@users.noreply.github.com>
2019-02-12 14:26:07 +0545

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,7 @@ router.post('/authenticate',(req,res,next)=>{
                if(err) throw err;
                if(isMatch){
                     const token = jwt.sign({data: user}, config.secret, {
-                         expiresIn: 1900 //in seconds
+                         expiresIn: 120 //in seconds
                     });
                     res.json({
                          sucess:true, 
